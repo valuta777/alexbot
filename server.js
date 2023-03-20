86907329066eb7cfd2d8f14e2c2c330b1ce56db2
Valentin Miheev <valentin.miheev@jetsoftpro.com>
2023-03-20 12:03:34 +0200

    fix2

--- server.js
+++ server.js
@@ -1,51 +1,68 @@
-/**
- * This example demonstrates setting up a webook, and receiving
- * updates in your express app
- */
-/* eslint-disable no-console */
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
 
 const TOKEN = process.env.TELEGRAM_TOKEN;
 const url = process.env.URL || 'localhost';
 const port = process.env.PORT || 8081;
 const answerUsername = process.env.USERNAME = '@kekassssss';
 
-const TelegramBot = require('node-telegram-bot-api');
-const express = require('express');
+
 
 // No need to pass any parameters as we will handle the updates with Express
 const bot = new TelegramBot(TOKEN);
 
-// This informs the Telegram servers of the new webhook.
-bot.setWebHook(`${url}/bot${TOKEN}`);
-
 const app = express();
+
+// This informs the Telegram servers of the new webhook.
+const setupWebhook = async () => {
+  try {
+    const result = await bot.setWebHook(`${url}/bot${TOKEN}`, {
+      certificate: './crt.pem', // Path to your crt.pem
+    });
+    console.log('webhook setup', result);
+  } catch (error) {
+    return error
+  }
+}
 
 // parse the updates to JSON
 app.use(express.json());
 
 // We are receiving updates at the route below!
 app.post(`/bot${TOKEN}`, (req, res) => {
-  console.log(`Receiving updates ${JSON.stringify(req.body)}`);
+  console.log('Receiving updates', req.body);
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
 
 // Start Express Server
-app.listen(port, () => {
+app.listen(port, async () => {
+  try {
+    await setupWebhook();
+  } catch (error) {
+    console.error(error);
+  }
   console.log(`Express server is listening on ${url} ${port}`);
 });
 
 // Just to ping!
-bot.on('message', msg => {
+bot.on('message', async (msg) => {
   const { message_id: originalMessageId, from: { username }, chat: { id: chatId, title} } = msg;
 
-  console.log(`Receiving message from ${username} in chat ${title}`);
+  console.log(`Receiving message`, msg);
 
   if (username !== answerUsername) {
     return;
   }
 
-  bot.sendMessage(chatId, `Пішов нахуй ${username}`, {
-    reply_to_message_id: originalMessageId
-  });
+  console.log(`Sending message to ${username}`, msg);
+
+  try {
+    const result = await bot.sendMessage(chatId, `Пішов нахуй ${username}`, {
+      reply_to_message_id: originalMessageId
+    })
+    console.log('Send message', result);
+  } catch (error) {
+    console.error(error);
+  };
 });
